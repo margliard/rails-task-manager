b3d22934c8684ce1d89b48b268f804e6db96ae26
Margot Liard <margliard@gmail.com>
2021-11-09 15:31:06 +0100

    rails2 task manager done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_100636) do
+ActiveRecord::Schema.define(version: 2021_11_09_105608) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
